11919face88dc1b9613dbe7b50ab767fb5ad56c6
Teeka <teeka@Cabrini.local>
2015-01-26 12:52:03 -0800

    Start cleaning up code.

--- ddupd.sh
+++ ddupd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+DEBUG=0
 
 #
 # ddupd
@@ -8,110 +13,131 @@
 #
 
 # Set to local configuration directory
-CONFDIR=.
+CONFDIR=/etc
 CONF="$CONFDIR/ddupd.conf"
 
 . $CONF
 
-IP4=`curl -4 -s "http://v4.ipv6-test.com/api/myip.php"`
+IP4=$( curl -4 -s "http://v4.ipv6-test.com/api/myip.php" )
 
 ##
 ## Logging
-DDUPDLOG="/Library/Logs/ddupd.log"
-find $DDUPDLOG -size +64k -exec rm -f {} \;
-touch $DDUPDLOG
+##
+LOGFILE="/Library/Logs/ddupd.log"
+find $LOGFILE -size +64k -exec rm -f {} \;
+touch $LOGFILE
 ##
 
-STDIN=0
+function db() {
+	STDIN=0
+	if [[ $DEBUG -eq 1 || -t "$STDIN" || -p /dev/stdin ]]; then
+		echo "$@"
+	fi
+}
 
-if [[ -t "$STDIN" || -p /dev/stdin ]]
-then
-  echo interactive
-else
-  echo non-interactive
-fi
+function log() {
+	ts="$(date +'%Y-%m-%d %H:%M:%S'): [$$]"
+	if [ $# -gt 0 ]; then
+		db "$@"
+		echo "$ts : $@" >> $LOGFILE
+	else
+		while read msg
+		do
+			db "$msg"
+			echo "$ts : $msg" >> $LOGFILE
+		done
+	fi
+}
 
 if [ "${#IP4}" -lt 7 ]; then
-	echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : Our IP was reported as '$IP4'. Not continuing." >> $DDUPDLOG
+	log "WARN : Our IP was reported as '$IP4'. Not continuing."
 	exit 0
 fi
 
 if [ -f /tmp/ddupd_ip4 ]; then
-	PP4=`cat /tmp/ddupd_ip4`
+	PP4=$(cat /tmp/ddupd_ip4)
 else
 	PP4=""
 fi
 
-if [ "$IP4" = "$PP4" ]; then
-	TIME=`date '+%H%M%S'`
-	# If it's between 0500 and 0501, force a heartbeat update.
-	# This assumes we're being called every minute.
-	if [ $TIME -lt 50000 ] || [ $TIME -gt 50100 ]; then
-		TIME=`date '+%M'`
-		if [ `expr $TIME % $LOGINTERVAL` -eq 0 ]; then
-			echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : BEAT : IP unchanged ($IP4)." >> $DDUPDLOG
-			exit 0
+STDIN=0
+if [[ $DEBUG -eq 1 || -t "$STDIN" || -p /dev/stdin ]]; then
+	log "TEST : Run interactively."
+else
+	if [ "$IP4" = "$PP4" ]; then
+		TIME=$(date '+%H%M%S')
+		# If it's between 0500 and 0501, force a heartbeat update.
+		# This assumes we're being called every minute.
+		if [ $TIME -lt 50000 ] || [ $TIME -gt 50100 ]; then
+			TIME=$(date '+%M')
+			if [ $(expr $TIME % $LOGINTERVAL) -eq 0 ]; then
+				log "BEAT : IP unchanged ($IP4)."
+				exit 0
+			else
+				exit 0
+			fi
 		else
-			exit 0
+			log "BEAT : Pushing $IP4 as our current address."
 		fi
-	else
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : BEAT : Pushing $IP4 as our current address." >> $DDUPDLOG
 	fi
 fi
 
-# Set Tunnelbroker IP
-if [ $TBENABLE = 1 ]; then
-	TBOUT=`curl -4 -k -s "https://$TBUSER:$TBPASS@ipv4.tunnelbroker.net/nic/update?hostname=$TBTID&myip=$IP4"`
+function update_TB() {
+	local user=$1
+	local pass=$2
+	local tid=$3
+	local ip4=$4
+	local url="https://${user}:${pass}@ipv4.tunnelbroker.net/nic/update"
+	TBOUT=$(curl -4 -k -s "${url}?hostname=${tid}&myip=${ip4}")
 	if [[ "$TBOUT" =~ "good" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : OKAY : [TB] Updated tunnel $TBTID to $IP4." >> $DDUPDLOG
+		log "OKAY : [TB] Updated tunnel $TBTID to $IP4."
 	elif [[ "$TBOUT" =~ "nochg" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [TB] Sent an update to Tunnelbroker, but IP for tunnel $TBTID was already $IP4." >> $DDUPDLOG
+		log "WARN : [TB] Sent an update to Tunnelbroker, but IP for tunnel $TBTID was already $IP4."
 	else
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [TB] $TBOUT" >> $DDUPDLOG
+		log "WARN : [TB] $TBOUT"
 	fi
+}
+
+function update_HE() {
+	local dom=$1
+	local pass=$2
+	local ip4=$3
+	local url="https://dyn.dns.he.net/nic/update"
+	FDOUT=$(curl -4 -k -s "${url}?hostname=${dom}&password=${pass}&myip=${ip4}")
+	if [[ "$FDOUT" =~ "good" ]]; then
+		log "OKAY : [HE] Updated Hurricane Electric domain ${dom} to ${ip4}."
+	elif [[ "$FDOUT" =~ "nochg" ]]; then
+		log "WARN : [HE] Sent an update to Hurricane Electric, but IP for ${dom} was already ${ip4}."
+	else
+		log "WARN : [HE] $FDOUT"
+	fi
+}
+
+# Set Tunnelbroker IP
+if [[ $TBENABLE -eq 1 ]]; then
+	update_TB $TBUSER $TBPASS $TBTID $IP4
 fi
 
 # Set Hurricane Electric
-if [ $HEENABLE1 = 1 ]; then
-	FDOUT=`curl -4 -k -s "https://dyn.dns.he.net/nic/update?hostname=$HEDOMN1&password=$HEPASS1&myip=$IP4"`
-	if [[ "$FDOUT" =~ "good" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : OKAY : [HE] Updated Hurricane Electric domain $HEDOMN1 to $IP4." >> $DDUPDLOG
-	elif [[ "$FDOUT" =~ "nochg" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] Sent an update to Hurricane Electric, but IP for $HEDOMN1 was already $IP4." >> $DDUPDLOG
-	else
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] $FDOUT" >> $DDUPDLOG
-	fi
+if [[ $HEENABLE1 -eq 1 ]]; then
+	update_HE $HEDOMN1 $HEPASS1 $IP4
 fi
-if [ $HEENABLE2 = 1 ]; then
-  FDOUT=`curl -4 -k -s "https://dyn.dns.he.net/nic/update?hostname=$HEDOMN2&password=$HEPASS2&myip=$IP4"`
-  if [[ "$FDOUT" =~ "good" ]]; then
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : OKAY : [HE] Updated Hurricane Electric domain $HEDOMN2 to $IP4." >> $DDUPDLOG
-  elif [[ "$FDOUT" =~ "nochg" ]]; then
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] Sent an update to Hurricane Electric, but IP for $HEDOMN2 was already $IP4." >> $DDUPDLOG
-  else
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] $FDOUT" >> $DDUPDLOG
-  fi
+if [[ $HEENABLE2 -eq 1 ]]; then
+	update_HE $HEDOMN2 $HEPASS2 $IP4
 fi
-if [ $HEENABLE3 = 1 ]; then
-  FDOUT=`curl -4 -k -s "https://dyn.dns.he.net/nic/update?hostname=$HEDOMN3&password=$HEPASS3&myip=$IP4"`
-  if [[ "$FDOUT" =~ "good" ]]; then
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : OKAY : [HE] Updated Hurricane Electric domain $HEDOMN3 to $IP4." >> $DDUPDLOG
-  elif [[ "$FDOUT" =~ "nochg" ]]; then
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] Sent an update to Hurricane Electric, but IP for $HEDOMN3 was already $IP4." >> $DDUPDLOG
-  else
-    echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [HE] $FDOUT" >> $DDUPDLOG
-  fi
+if [[ $HEENABLE3 -eq 1 ]]; then
+	update_HE $HEDOMN3 $HEPASS3 $IP4
 fi
 
 # Set DNS-O-Matic
 if [ $DMENABLE = 1 ]; then
-	DMOUT=`curl -4 -k -s "https://$DMUSER:$DMPASS@updates.dnsomatic.com/nic/update?myip=$IP4"`
+	DMOUT=$(curl -4 -k -s "https://$DMUSER:$DMPASS@updates.dnsomatic.com/nic/update?myip=$IP4")
 	if [[ "$DMOUT" =~ "good" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : OKAY : [DM] Updated DNS-O-Matic to $IP4." >> $DDUPDLOG
+		log "OKAY : [DM] Updated DNS-O-Matic to $IP4."
 	elif [[ "$DMOUT" =~ "nochg" ]]; then
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [DM] Sent an update to DNS-O-Matic, but IPs were already $IP4." >> $DDUPDLOG
+		log "WARN : [DM] Sent an update to DNS-O-Matic, but IPs were already $IP4."
 	else
-		echo "`date  +'%Y-%m-%d %H:%M:%S'`: [$$] : WARN : [DM] $DMOUT" >> $DDUPDLOG
+		log "WARN : [DM] $DMOUT"
 	fi
 fi
 
